1034b277ce1c04d3d6a8532f94838bcfc76e1d54
dimapopov <dima1popov@ukr.net>
2016-01-17 23:23:51 +0300

    0.2.0

--- orna-0.2.0.js
+++ orna-0.2.0.js
@@ -1,3 +1,4 @@
+//https://github.com/OrnaOrg/OrnaJS
 //alert("TEST OK!");
 //------------------reverse-background-----------------------------
 //reversebackground('id', 'url1', 'url2');
@@ -212,7 +213,7 @@ $(document).ready(function() {
     createatom();
 });
 
-function createatom() {
+function createatom(current) {
         var tag = ['div', 'body', 'p', 'form', 'button', 'img', 'input', 'a', 'ul', 'ol', 'li', 'select', 'option', 'span', 'table', 'main', 'nav', 'menu', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'textarea', 'fieldset', 'header', 'footer', 'code'];
         for (var i = 0; i !== tag.length; i++) {
             var tagsize = $(tag[i]).size();
@@ -250,15 +251,15 @@ function createatom() {
                                 part[0] = 'transform'
                                 addstyle(part, val);
                                 //alert(part[0]+part[1]);
-                            }}
-                            else if (part[0] == "scale") {
+                            }
+                        } else if (part[0] == "scale") {
                             if (val !== undefined) {
                                 part[1] = part[0] + '(' + part[1] + ')';
                                 part[0] = 'transform'
                                 addstyle(part, val);
                                 //alert(part[0]+part[1]);
                             }
-                            } else if (part[0] == "skew") {
+                        } else if (part[0] == "skew") {
                             if (val !== undefined) {
                                 part[1] = part[0] + '(' + part[1] + ')';
                                 part[0] = 'transform'
@@ -266,15 +267,14 @@ function createatom() {
                                 //alert(part[0]+part[1]);
                             }
 
-                        } else if (part[0] == "box-shadow"||part[0] == "shadow") {
+                        } else if (part[0] == "box-shadow" || part[0] == "shadow") {
                             if (val !== undefined) {
-                                 if(part[2]==undefined||part[3]==undefined)
-                                {
-                                    
-                                    alert("Hi! I'm Orna! Atomic class shadow or box-shadow need five value: shadow_0_0_20px_10px_black"); 
-                                    
+                                if (part[2] == undefined || part[3] == undefined) {
+
+                                    alert("Hi! I'm Orna! Atomic class shadow or box-shadow need five value: shadow_0_0_20px_10px_black");
+
                                 }
-                                part[0]="box-shadow";
+                                part[0] = "box-shadow";
                                 part[1] = part[1] + ' ' + part[2] + ' ' + part[3] + ' ' + part[4] + ' ' + part[5];
                                 addstyle(part, val);
                                 //alert(part[1]);
@@ -287,11 +287,10 @@ function createatom() {
                             }
                         } else if (part[0] == "border") {
                             if (val !== undefined) {
-                                if(part[2]==undefined||part[3]==undefined)
-                                {
-                                    
-                                    alert("Hi! I'm Orna! Atomic class border need three value width, color and style"); 
-                                    
+                                if (part[2] == undefined || part[3] == undefined) {
+
+                                    alert("Hi! I'm Orna! Atomic class border need three value width, color and style");
+
                                 }
                                 part[1] = part[1] + ' ' + part[2] + ' ' + part[3];
                                 addstyle(part, val);
@@ -299,11 +298,10 @@ function createatom() {
                             }
                         } else if (part[0] == "outline") {
                             if (val !== undefined) {
-                                   if(part[2]==undefined||part[3]==undefined)
-                                {
-                                    
-                                    alert("Hello! I'm Orna! Atomic class outline need three value: width, color, style"); 
-                                    
+                                if (part[2] == undefined || part[3] == undefined) {
+
+                                    alert("Hello! I'm Orna! Atomic class outline need three value: width, color, style");
+
                                 }
                                 part[1] = part[1] + ' ' + part[2] + ' ' + part[3];
                                 addstyle(part, val);
@@ -314,86 +312,154 @@ function createatom() {
                                 part[0] = 'background';
                                 addstyle(part, val);
                             }
-                        }
-                            else if (part[0] == "bgc") {
+                        } else if (part[0] == "bgc") {
                             if (val !== undefined) {
                                 part[0] = 'background-color';
                                 addstyle(part, val);
-                            }}
-                                else if (part[0] == "bgp") {
+                            }
+                        } else if (part[0] == "bgp") {
                             if (val !== undefined) {
                                 part[0] = 'background-position';
                                 addstyle(part, val);
                             }
-                            
-                        }     else if (part[0] == "bgr") {
+
+                        } else if (part[0] == "bgr") {
                             if (val !== undefined) {
                                 part[0] = 'background-repeat';
                                 addstyle(part, val);
-                            }}
-                            else if (part[0] == "bgi") {
+                            }
+                        } else if (part[0] == "bgi") {
                             if (val !== undefined) {
                                 part[0] = 'background-image';
                                 addstyle(part, val);
                             }
-                            
+
                         } else if (part[0] == "transition") {
-                           
-                                   if(part[2]==undefined||part[3]==undefined||part[4]==undefined)
-                                {
-                                    
-                                    alert("Hello! Atomic class transition need four value, like it: transition_all_0.5s_ease_2s"); 
-                                    
-                                }
+
+                            if (part[2] == undefined || part[3] == undefined || part[4] == undefined) {
+
+                                alert("Hello! Atomic class transition need four value, like it: transition_all_0.5s_ease_2s");
+
+                            }
                             if (val !== undefined) {
 
                                 part[1] = part[1] + ' ' + part[2] + ' ' + part[3] + ' ' + part[4];
 
                                 addstyle(part, val);
                             }
-                        }
- else if (part[0] == "Arial"||part[0] == "arial") {
-                
-                                part[0] = 'font-family';
-                                part[1]= 'Arial, sans-serif';
-                                extrastyle(part);
-                            
-                            
-                        } else if (part[0] == "Times"||part[0] == "TimesNewRoman") {
-                
-                                part[0] = 'font-family';
-                                part[1]= '"Times New Roman", serif';
-                                extrastyle(part);
-                            
-                            
-                        }
-                        else if (part[0] == "center") {
-                
-                                part[0] = 'margin';
-                                part[1]= 'auto';
-                                extrastyle(part);
-                            
-                            
+                        } else if (part[0] == "Arial" || part[0] == "arial") {
+
+                            part[0] = 'font-family';
+                            part[1] = 'Arial, sans-serif';
+                            extrastyle(part);
+
+
+                        } else if (part[0] == "Times" || part[0] == "TimesNewRoman") {
+
+                            part[0] = 'font-family';
+                            part[1] = '"Times New Roman", serif';
+                            extrastyle(part);
+
+
+                        } else if (part[0] == "center") {
+
+                            part[0] = 'margin';
+                            part[1] = 'auto';
+                            extrastyle(part);
+
+
                         } else if (part[0] == "textincenter") {
-                
-                                part[0] = 'text-align';
-                                part[1]= 'center';
-                                extrastyle(part);
-                            
-                            
-                        } 
+
+                            part[0] = 'text-align';
+                            part[1] = 'center';
+                            extrastyle(part);
+
+
+                        } else if (part[0] == "uppercase") {
+
+                            part[0] = 'text-transform';
+                            part[1] = 'uppercase';
+                            extrastyle(part);
+
+
+                        } else if (part[0] == "lowercase") {
+
+                            part[0] = 'text-transform';
+                            part[1] = 'lowercase';
+                            extrastyle(part);
+
+
+                        } else if (part[0] == "capitalize") {
+
+                            part[0] = 'text-transform';
+                            part[1] = 'capitalize';
+                            extrastyle(part);
+
+
+                        } else if (part[0] == "capitalize") {
+
+                            part[0] = 'text-transform';
+                            part[1] = 'capitalize';
+                            extrastyle(part);
+
+
+                        }
 
 
                         function extrastyle(part) {
-                            
-                                $(current).css(part[0], part[1]);
-                          
-                        }     function addstyle(part, val) {
+
+                            $(current).css(part[0], part[1]);
+
+                        }
+
+                        function addstyle(part, val) {
                             if (val !== undefined) {
                                 //alert('atom class');
-                                $(current).css(part[0], part[1]);
-                            } else {
-                                //alert('other class');
+
+
+                                if (part[2] == 'mouseover')
+
+                                {
+                                    //alert(part[2]); 
+                                    $(current).on('mouseenter', function() {
+                                        $(current).css(part[0], part[1]);
+                                    });
+
+                                } else if (part[2] == 'mouseout')
+
+                                {
+                                    //alert(part[2]); 
+
+                                    $(current).on('mouseleave', function() {
+                                        $(current).css(part[0], part[1]);
+
+                                    });
+                                } else if (part[2] == 'focus')
+
+                                {
+                                    //alert(part[2]); 
+                                    $(current).on('focusin', function() {
+                                        $(current).css(part[0], part[1]);
+                                    });
+
+                                } else if (part[2] == 'blur')
+
+                                {
+                                    //alert(part[2]); 
+
+                                    $(current).on('focusout', function() {
+                                        $(current).css(part[0], part[1]);
+                                    });
+                                } else if (part[2] == 'click')
+
+                                {
+                                    //alert(part[2]); 
+                                    $(current).click(function() {
+                                        $(current).css(part[0], part[1]);
+                                    });
+                                } else {
+                                    $(current).css(part[0], part[1]);
+                                }
                             }
                         }
 
